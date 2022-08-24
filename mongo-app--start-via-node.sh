7145bee4b867b3a2d30e62d0a1a30a43ac9f52bf
Eziz Nurmuhammedov <oasisindesert@gmail.com>
2022-08-24 20:18:26 +0500

    updated scripts

--- mongo-app--start-via-node.sh
+++ mongo-app--start-via-node.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 #
-docker run --name mongo-app --network mongo-net -d -p 3000:3000 -v /home/nurezi/demo-nana/app:/home/app node:18.7.0-alpine3.16 node /home/app/server.js
+docker run --name mongo-app --network mongo-net -d -p 3000:3000 -v /home/nurezi/demo-nana/app:/home/app node:13.14.0-alpine3.11 node /home/app/server.js
+# docker run --name mongo-app --network mongo-net -d -p 3000:3000 -v /home/nurezi/demo-nana/app:/home/app node:18.7.0-alpine3.16 node /home/app/server.js
   
